9e0ff11612d9439f6e117dcdd287e62a2dbc4cc7
tokuicchi <tokuicchi@gmail.com>
2024-04-05 21:10:33 +0900

    first commit

---
+++ src/pages/api/getPopularMovies.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+    try {
+        const response = await axios(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`);
+        res.status(200).json(response.data);
+        console.log('バックエンド結果：',response.data);
+    } catch(err) {
+        console.log(err);
+        res.status(500).json('エラーが発生しました',err);
+    }
+}
---
+++ src/pages/home.js
@@ -0,0 +1,44 @@
+import AppLayout from '@/components/Layouts/AppLayout'
+import axios from 'axios'
+import Head from 'next/head'
+import { useEffect } from 'react'
+
+const Home = () => {
+    useEffect(() => {
+        console.log(1);
+        const fetchMovies = async() => {
+            try {
+                const response = await axios.get('api/getPopularMovies');
+                console.log('フロントエンド結果：',response);
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        fetchMovies();
+    }, [])
+
+    return (
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Home
+                </h2>
+            }>
+            <Head>
+                <title>Laravel - Home</title>
+            </Head>
+
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 bg-white border-b border-gray-200">
+                            You're logged in!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AppLayout>
+    )
+}
+
+export default Home
