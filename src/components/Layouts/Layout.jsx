66a5f8926ade24e4b89b6862bb13640a30cec28a
tokuicchi <tokuicchi@gmail.com>
2024-04-20 06:13:10 +0900

    feat: 検索画面のレイアウト準備

---
+++ src/components/Layouts/Layout.jsx
@@ -0,0 +1,23 @@
+import { Box, Container, Grid } from '@mui/material'
+import React from 'react'
+
+const Layout = ({children,sidebar}) => {
+  return (
+    <Container>
+        <Grid container spacing={3} py={4}>
+            <Grid item xs={12} md={3}>
+                <Box bgcolor={"white"} boxShadow={1}>
+                    {/* サイドバー */}
+                    {sidebar}
+                </Box>
+            </Grid>
+        
+            <Grid item xs={12} md={9}>
+                {children}
+            </Grid>
+        </Grid>
+    </Container>
+  )
+}
+
+export default Layout
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,4 +1,10 @@
+import AppLayout from '@/components/Layouts/AppLayout';
+import Layout from '@/components/Layouts/Layout';
+import MediaCard from '@/components/MediaCard';
+import Sidebar from '@/components/Sidebar';
+import { Grid } from '@mui/material';
 import axios from 'axios';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react'
 import { useState } from 'react';
@@ -29,8 +35,23 @@ function search() {
         fetchMedia()
 
     }, [searchQuery])
+
   return (
-    <div>search</div>
+    <AppLayout
+        header={
+            <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                Search
+            </h2>
+        }>
+        <Head>
+            <title>Laravel - Search</title>
+        </Head>
+    <Layout sidebar={<Sidebar />}>
+        <Grid container spacing={3}>
+            <MediaCard />
+        </Grid>
+    </Layout>
+    </AppLayout>
   )
 }
 
